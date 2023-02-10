1b3840c475bd832bcb9f8c27d7984196a3c1fd6d
jitendrapitchuka <jithendra2900@gmail.com>
2023-02-10 23:21:07 +0530

    library frontend going

---
+++ library-app/03-frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './layouts/NavbarAndFooter/Footer';
+import Navbar from './layouts/NavbarAndFooter/Navbar';
+import Homepage from './layouts/Homepage/Homepage';
+
+function App() {
+  return (
+    <>
+   <Navbar/>
+   <Homepage/>
+   <Footer/>
+   </>
+  );
+}
+
+export default App;
